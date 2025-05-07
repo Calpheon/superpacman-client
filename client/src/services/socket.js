717b347fea66af65584ec3361b2ab8fe8beec0b0
hafizhabdul <abdulhafizhsaenal@gmail.com>
2025-05-07 17:31:46 +0700

    fix: update url socket after deploy server

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("http://3.25.190.177", {
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
   autoConnect: true,
